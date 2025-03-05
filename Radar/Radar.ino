041a67eb70f43d003e646e6719b1bf0754b6b69d
SpaceLink <128892453+Andrew-24coop@users.noreply.github.com>
2025-03-05 11:43:49 +0300

    Update Radar.ino

--- Radar/Radar.ino
+++ Radar/Radar.ino
@@ -1,21 +1,21 @@
 #include <Servo.h>
-Servo myservo;
+Servo myservo; // подключение серво как myservo
 #define HC_TRIG 3
 #define HC_ECHO 2
 float distFilt = 0;
-int pos = 23;
-bool direction = true;
+int pos = 23; //начальная позиция
+bool direction = true; // изначальное напрваление движения 
 void setup() {
-  Serial.begin(9600);
-  pinMode(HC_TRIG, OUTPUT);
-  pinMode(HC_ECHO, INPUT);
-  myservo.attach(4);
+  Serial.begin(9600); // подключение порта на частоте 9600 бод (бит/сек)
+  pinMode(HC_TRIG, OUTPUT); // назначение пина TRIG на вывод
+  pinMode(HC_ECHO, INPUT);  // назначение пина ECHO на ввод
+  myservo.attach(4); // подключение серво на пин 4
   delay(500);
 }
 
 void loop() {
-  float dist = getDist();
-  distFilt += (dist - distFilt) * 0.2;
+  float dist = getDist(); // получение сырого расстояния
+  distFilt += (dist - distFilt) * 0.2; // фильтрация
 
   if (direction && pos <= 157) {
     pos++;
@@ -34,7 +34,7 @@ void loop() {
   delay(50);
 }
 
-float getDist() {
+float getDist() { // функция получения сырого значения с датчика
   digitalWrite(HC_TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(HC_TRIG, LOW);
